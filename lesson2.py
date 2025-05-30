38811b2e1dd95db85568f496c544a2e898d7423f
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2025-05-30 21:08:06 +0800

    first commit

---
+++ lesson2.py
@@ -0,0 +1,22 @@
+
+#lesson2
+
+
+#ex1
+print("hello world")
+print("lesson 2")
+
+#ex2
+print("welcome to course python")
+
+
+#ex3
+# this new comment
+#print("this is new comment")
+
+""" 
+Indentation refers to the spaces at the beginning of a code line.
+Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+Python uses indentation to indicate a block of code.
+"""
+
