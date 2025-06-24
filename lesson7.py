b95fdab8ea445e6b46010ad5396f88e75181dcd1
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2025-06-24 20:51:12 +0800

    lesson7

---
+++ lesson7.py
@@ -0,0 +1,92 @@
+#lesson 7
+##ex1
+##capitalize
+x="mohammad ilyas"
+print(x.capitalize())
+
+#ex2
+x="mohammad ilyas STUDENT"
+b=x.capitalize()
+print(b)
+
+
+#ex3
+##casefold()
+adel="trade With shop BABE"
+string_adel=adel.casefold()
+print(string_adel)
+
+
+#ex4
+##center
+# 
+text="apple"
+g_center=text.center(20)
+print(g_center)
+
+
+#ex5
+##center
+# 
+text="apple orange"
+g_center=text.center(20)
+print(g_center)
+
+
+#ex6
+##center
+# 
+text="apple orange"
+g_center=text.center(10)
+print(g_center)
+
+
+#ex7
+##center
+# 
+text="apple orange"
+g_center=text.center(20,"*")
+print(g_center)
+
+
+
+
+#ex8
+##center
+# 
+text="apple orange"
+g_center=text.center(40,"*")
+print(g_center)
+
+
+
+
+#ex9
+##count
+# 
+text="1 apple orange 2 apple orange 3 apple orange 4 apple orange "
+g_count=text.count("apple")
+print(g_count)
+
+
+
+#ex10
+##count 
+# 
+text="1 apple orange 2 apple orange 3 apple orange 4 apple orange "
+g_count=text.count("apple",1,25)
+print(g_count)
+
+#ex11
+##count 
+# 
+text="1 apple orange 2 apple orange 3 apple orange 4 apple orange "
+g_count=text.count("fruits")
+print(g_count)
+
+#ex12
+##count 
+# 
+text="1 apple orange 2 apple orange 3 apple orange 4 apple orange "
+g_count=text.count("ap ple")
+print(g_count)
