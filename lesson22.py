75ee41d057b6eea69fd6c29c9d8a767b227dc89e
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2025-08-15 21:52:05 +0800

    lessn 22

--- lesson22.py
+++ lesson22.py
@@ -85,3 +85,57 @@ print(dddd)
 def fun_nationality(v):
     pass
 
+
+
+##ex10
+def fun_1(v,/):
+    print(v)
+   
+fun_1(6)
+#fun_1(v=7)
+
+def fun_1(v):
+    print(v)
+   
+fun_1(6)
+fun_1(v=7)
+##ex11
+def fun_1(*,v):
+    print(v)
+#fun_1(6)
+fun_1(v=7)
+##ex12
+def fun_1(name,laname,/,*,family,age):
+    print(name+laname+family+age)
+fun_1("Salwa","Jamal",family="555",age="33")   
+###ex13
+##n!=n * n-1  * n-2 * .....  * 3 *2 *1
+### 0!=1                       1!=1             2!= 2 * 1          3!=3 * 2 *1 =6       4!=4*3*2*1=24 
+## 9! = 9*8*7*6*5*4*3*2*1
+def fact(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n * fact(n-1)
+print(fact(0))
+print(fact(1))
+print(fact(2))
+print(fact(3))
+print(fact(4))
+##ex14
+#lambda function
+## lambda arguments:expression
+## أضف 16 الى الوسيط جي وقم بارجاع النتيجة في المتغير واي
+y=lambda g:g+16
+print(y(2))
+##ex15
+#lambda function
+## lambda arguments:expression
+mul=lambda g,k:g * k
+print(mul(2,3))
+sum=lambda g,k:g + k
+print(sum(2,3))
+sub=lambda g,k,l:g - k - l
+print(sub(2,3,1))
+
+
