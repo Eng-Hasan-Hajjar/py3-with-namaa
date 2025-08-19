a4094fe9cfd8a436bc36d2289518156e0f1ca49e
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2025-08-19 22:08:47 +0800

    lesson23

--- lesson23.py
+++ lesson23.py
@@ -80,3 +80,132 @@ print("name",p2.name,"age",p2.age)
 print(p1)
 print(p2)
 
+##ex6
+class Person:
+    def __init__(self,name,age,father,mother,id):
+        self.name=name
+        self.age=age
+        self.father=father
+        self.mother=mother
+        self.id=id
+    def __str__(self):
+        return self.father +" "+ self.name
+    
+    def info(self):
+        print("my name is :"+ self.name)
+
+p1=Person("Ilyas",60,"Hamed","Hamida",13245698)
+p1.info()
+
+
+##ex7
+class Person:
+    def __init__(self,name,age,father,mother,id):
+        self.name=name
+        self.age=age
+        self.father=father
+        self.mother=mother
+        self.id=id
+    def __str__(self):
+        return self.father +" "+ self.name
+    
+    def info(ggg):
+        print("my name is :"+ ggg.name)
+
+p1=Person("Ilyas",60,"Hamed","Hamida",13245698)
+p1.info()
+
+
+
+##ex8
+class Person:
+    def __init__(self,name,age,father,mother,id):
+        self.name=name
+        self.age=age
+        self.father=father
+        self.mother=mother
+        self.id=id
+    def __str__(self):
+        return self.father +" "+ self.name
+    
+    def info(ggg):
+        print("my name is :"+ ggg.name)
+
+
+    def get_age(ggg):
+        print("my age is :"+ str(ggg.age))
+
+p1=Person("Ilyas",60,"Hamed","Hamida",13245698)
+p1.age=22
+print(p1)
+p1.get_age()
+
+
+##ex9
+class Person:
+    def __init__(self,name,age,father,mother,id):
+        self.name=name
+        self.age=age
+        self.father=father
+        self.mother=mother
+        self.id=id
+    def __str__(self):
+        return self.father +" "+ self.name
+    
+    def info(ggg):
+        print("my name is :"+ ggg.name)
+
+
+    def get_age(ggg):
+        print("my age is :"+ str(ggg.age))
+
+p1=Person("Ilyas",60,"Hamed","Hamida",13245698)
+p1.age=22
+print(p1)
+p1.get_age()
+del p1.age
+#p1.get_age()
+del p1
+
+##ex10
+class Car:
+     pass
+
+
+
+##ex11
+class Player:
+    def __init__(self,name,health=100):
+          self.name=name
+          self.health=health
+          self.inventory=[]
+          self.position=(0,0)
+    def move(self,xx,yy):
+        self.position=(xx,yy)
+        print(f"{self.name} moved to : {self.position}")
+
+    def take_damage(self,damage):
+         self.health-=damage
+         if self.health <= 0:
+              print(f"{self.name} has been eliminated!")
+         else:
+              print(f"{self.name} took {damage} ,{self.health} health remaining")   
+    def pick_up_weapon(self,weapon):
+         self.inventory.append(weapon)
+         print(f"{self.name} picked up {weapon}")
+
+
+player1=Player("Asmaa")  
+player2=Player("Watheq")       
+
+player1.move(10,12)
+player2.move(55,300)
+
+
+player1.pick_up_weapon("AKM")
+player2.pick_up_weapon("M4")
+
+
+player2.take_damage(20)
+player2.take_damage(30)
+player1.take_damage(220)
